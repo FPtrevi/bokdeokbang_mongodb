d2959544082bae1e431a255cf88b1ed16031f2f7
chan <dhguscks20@naver.com>
2024-09-11 00:13:48 +0900

    add constants

---
+++ src/constants/formdata.js
@@ -0,0 +1,43 @@
+const FORM_DATA = {
+  house_no: "",
+  member_no: "", // session
+  title: "",
+  description: "",
+  thumnail: "",
+  sales_type: "",
+  address1: "",
+  address2: "",
+  zip_code: "",
+  building_type: "",
+  building_floor: "",
+  deposit: "",
+  monthly_cost: "",
+  manage_cost: "",
+  floor: "",
+  room_type: "",
+  room_count: "",
+  direction: "",
+  parking: "",
+  lat: "", //시스템
+  lng: "", //시스템
+  approve_date: "",
+  size_p: "",
+  size_m2: "",
+  pet: "",
+  loan: "",
+  movein_date: "",
+  reg_date: "", // 시스템
+  view_count: "", //시스템
+  report_count: "", // 시스템
+  sales_status: "",
+  option_info: "",
+  shortterm: "",
+  fulloption: "",
+  elevator: "",
+  veranda: "",
+  security: "",
+  charge: "",
+  images: [],
+};
+
+export default FORM_DATA;
